fa8715378067203f0f699e50c835f3f8512db115
JVCA101 <joaovictor.correa@estudante.ufjf.com>
2025-07-24 16:36:10 -0300

    Adicionando executar_quantum em Processo

--- Processo.hpp
+++ Processo.hpp
@@ -9,13 +9,14 @@ class Processo
 private:
     int id;
     unsigned long tempoDeExecucao; // em segundos
+    unsigned long tempoRestante;
 
 public:
 
     Processo(const int id, const unsigned long tempo)
     {
         this->id = id;
-        this->tempoDeExecucao = tempo;
+        this->tempoRestante = this->tempoDeExecucao = tempo;
     }
 
     int getId() const noexcept
@@ -49,11 +50,43 @@ public:
     {
         std::cout << "Executando processo " << this->id << "\n";
         const auto start = std::chrono::high_resolution_clock::now();
-        std::this_thread::sleep_for(std::chrono::seconds(tempoDeExecucao));
+        std::this_thread::sleep_for(std::chrono::seconds(this->tempoDeExecucao));
         const auto end = std::chrono::high_resolution_clock::now();
 
         std::cout << "Processo " << this->id 
                   << " terminou de executar em " << std::chrono::duration_cast<std::chrono::seconds>(end-start).count()
                   << " segundos\n";
+        
+    }
+
+    bool executar_quantum(const unsigned long quantum)
+    {
+        unsigned long time_to_run;
+        if(quantum < tempoRestante)
+        {
+            time_to_run = tempoRestante;
+            tempoRestante = 0;
+        }
+        else
+        {
+            time_to_run = quantum;
+            tempoRestante = tempoRestante - quantum;
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(time_to_run));
+        
+        bool still_running = true;
+        if(tempoRestante == 0)
+        {
+            std::cout << "Processo " << this->id << "terminou de executar";
+            return !still_running;
+        }
+        else
+        {
+            std::cout << "Executando processo " << this->id 
+                      << "\nTempo restante: " << tempoRestante;
+            return still_running;
+        }
+        
     }
 };
